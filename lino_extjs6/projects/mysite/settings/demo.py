8e42d19e1298e293a83c19d515c5f7874c804b99
Luc Saffre <luc.saffre@gmail.com>
2016-01-27 13:16:22 +0200

    http://luc.lino-framework.org/blog/2016/0127.html

--- lino_extjs6/projects/mysite/settings/demo.py
+++ lino_extjs6/projects/mysite/settings/demo.py
@@ -6,7 +6,7 @@ from lino_extjs6.projects.mysite.settings import *
 class Site(Site):
     project_name = 'extjs_demo'
     is_demo_site = True
-    ignore_dates_after = datetime.date(2019, 05, 22)
+    # ignore_dates_after = datetime.date(2019, 05, 22)
     the_demo_date = datetime.date(2015, 03, 12)
 
 SITE = Site(globals())
