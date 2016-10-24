346f726a12078e7667ef32808feda0618f568839
khchine5 <hamzakhchine@gmail.com>
2016-10-24 06:39:14 +0100

    Add support to Py3 for datetime

--- lino_extjs6/projects/mysite/settings/demo.py
+++ lino_extjs6/projects/mysite/settings/demo.py
@@ -7,7 +7,7 @@ class Site(Site):
     project_name = 'extjs_demo'
     is_demo_site = True
     # ignore_dates_after = datetime.date(2019, 05, 22)
-    the_demo_date = datetime.date(2015, 03, 12)
+    the_demo_date = datetime.date(2015, 3, 12)
 
 SITE = Site(globals())
 
