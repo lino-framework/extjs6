26a6da97598225de114063dbf6dd32bf524ea546
Luc Saffre <luc.saffre@gmail.com>
2017-02-10 20:19:47 +0200

    http://luc.lino-framework.org/blog/2017/0210.html

--- tasks.py
+++ tasks.py
@@ -5,6 +5,6 @@ ns.setup_from_tasks(
     # tolerate_sphinx_warnings=True,
     blogref_url = 'http://luc.lino-framework.org',
     revision_control_system='git',
-    locale_dir='lino_extjs/extjs/locale',
+    # locale_dir='lino_extjs/extjs/locale',
     cleanable_files=['docs/api/lino_extjs6.*'],
     demo_projects=['lino_extjs6.projects.mysite.settings.demo'])
