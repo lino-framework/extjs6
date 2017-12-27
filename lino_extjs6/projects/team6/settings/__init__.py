75018d227ff576b395d06bf9f77960e65200b7c7
Luc Saffre <luc.saffre@gmail.com>
2017-12-27 11:31:59 +0200

    third series of changes

--- lino_extjs6/projects/team6/settings/__init__.py
+++ lino_extjs6/projects/team6/settings/__init__.py
@@ -2,7 +2,7 @@ from lino_book.projects.team.settings import *
 
 
 class Site(Site):
-    default_ui = 'lino_extjs6.extjs6'
+    default_ui = 'lino_extjs6.extjs'
     project_name = "extjs6_team"
     title = "Team Lino ExtJS 6 demo"
     languages = ['en', 'fr', 'de']
