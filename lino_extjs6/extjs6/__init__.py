b13694546c2f18ff6907ea23353a1ec090981e08
Luc Saffre <luc.saffre@gmail.com>
2017-12-27 06:44:17 +0200

    second series of changes

--- lino_extjs6/extjs6/__init__.py
+++ lino_extjs6/extjs6/__init__.py
@@ -24,8 +24,8 @@ your application.
 from __future__ import unicode_literals
 from __future__ import print_function
 
-from lino.api.ad import Plugin
 from django.utils.translation import ugettext_lazy as _
+from lino.modlib.extjs import Plugin
 
 
 class Plugin(Plugin):
@@ -36,8 +36,6 @@ class Plugin(Plugin):
 
     """
 
-    needs_plugins = ['lino.modlib.bootstrap3']
-
     disables_plugins = ['tinymce', 'extensible']
     """These two plugins don't work with ExtJS6 and therefore will
     automatically disabled if you set :attr:`default_ui` to
@@ -45,72 +43,7 @@ class Plugin(Plugin):
 
     """
 
-    enter_submits_form = False
-    """Whether the :kbd:`ENTER` key (or :kbd:`CTRL+ENTER` when in a
-    textarea field) should submit the form.
-
-    The default is `False`. For sites that were in production before
-    October 2015, we recomment to explain to the users that
-    :kbd:`Ctrl-S` is the preferred keyboard shortcut for submitting a
-    form. Alternatively for backwards compatibility you can set it to
-    `True` using something like this::
-
-        def setup_plugins(self):
-            super(Site, self).setup_plugins()
-            if self.is_installed('extjs6'):
-                self.plugins.extjs6.configure(enter_submits_form=False)
-
-
-    This setting is read by :xfile:`linoweb.js` who adds the exotic
-    special mapping for :kbd:`ENTER` only when this is `True`.
-
-    """
-    ui_label = _("Admin")
-
-    use_statusbar = False
-    """
-    Whether to use a status bar to display certain messages to the user.
-    Default is `False` since currently this is not really useful.
-    """
-
-    url_prefix = "ext"
-
     media_name = 'ext-6.2.0'
-
-    # media_base_url = "http://extjs-public.googlecode.com/" + \
-    #                  "svn/tags/extjs-6.0.0/release/"
-    """The URL from where to include the ExtJS library files.
-    
-    The default value points to the `extjs-public
-    <http://code.google.com/p/extjs-public/>`_ repository and thus
-    requires the clients to have an internet connection.  This
-    relieves newcomers from the burden of having to specify a download
-    location in their :xfile:`settings.py`.
-    
-    On a production site you'll probably want to download and serve
-    these files yourself by setting this to `None` and setting
-    :attr:`extjs_root` (or a symbolic link "extjs" in your
-    :xfile:`media` directory) to point to the local directory where
-    ExtJS 3.3.1 is installed).
-
-    """
-
-    autorefresh_seconds = 0
-    """Number of seconds to wait between two refreshes when autorefresh is
-    activated. Default is 60. Set this to 0 in order to deactivate
-    the autorefresh button.
-
-    """
-
-    media_root = None
-    """
-    Path to the ExtJS root directory.  Only used when
-    :attr:`media_base_url` is None, and when the `media` directory has
-    no symbolic link named `extjs` pointing to the ExtJS root
-    directory.
-    """
-
-    ui_handle_attr_name = 'extjs_handle'
 
     select_theme = False
     """Whether to let the user choose his preferred theme.
@@ -140,67 +73,11 @@ class Plugin(Plugin):
 
         # logger.info("20140227 extjs.Plugin.on_ui_init() b")
 
-    def get_row_edit_lines(self, e, panel):
-        from lino_extjs6.extjs6.elems import (GridElement, HtmlBoxElement,
-                                             FieldElement, form_field_name)
-        from lino.core import constants
-        master_field = panel.layout_handle.layout._datasource.master_field
-        if isinstance(e, GridElement):
-            yield "%s.on_master_changed();" % e.as_ext()
-        elif isinstance(e, HtmlBoxElement):
-            yield "%s.refresh();" % e.as_ext()
-        elif isinstance(e, FieldElement):
-            holder = panel.layout_handle.layout.get_chooser_holder()
-            chooser = holder.get_chooser_for_field(e.field.name)
-            if not chooser:
-                return
-            for f in chooser.context_fields:
-                if master_field and master_field.name == f.name:
-                    yield "var bp = this.get_base_params();"
-                    yield "%s.setContextValue('%s',bp['%s']);" % (
-                        e.as_ext(), constants.URL_PARAM_MASTER_PK,
-                        constants.URL_PARAM_MASTER_PK)
-                    yield "%s.setContextValue('%s',bp['%s']);" % (
-                        e.as_ext(), constants.URL_PARAM_MASTER_TYPE,
-                        constants.URL_PARAM_MASTER_TYPE)
-                else:
-                    yield (
-                        "%s.setContextValue('%s', record ? record."
-                        "data['%s'] : undefined);" % (
-                            e.as_ext(), f.name, form_field_name(f)))
-
-    # def get_css_includes(self, site):
-        # yield self.build_lib_url('resources/css/ext-all.css')
+    def get_css_includes(self, site):
+        return []
 
     def get_js_includes(self, settings, language):
         return []
-
-    def get_head_lines(self, site, request):
-        yield "<style>"
-        from lino.core.constants import ICON_NAMES
-        tpl = ".x-tbar-{0}{{ background-image: url({1}) !important; }}"
-        for n in ICON_NAMES:
-            url = site.build_static_url('images', 'mjames', n + '.png')
-            yield tpl.format(n, url)
-            
-        yield """
-.x-tbar-done{ background-image: url(/static/images/mjames/accept.png) !important; }
-.x-tbar-parameters{ background-image: url(/static/images/mjames/database_gear.png) !important; }
-"""
-        yield "</style>"
-
-    def get_used_libs(self, html=False):
-        if html is not None:
-            # version = '<script type="text/javascript">\
-            #    document.write(Ext.version);</script>'
-            onclick = "alert('ExtJS client version is ' + Ext.version);"
-            tip = "Click to see ExtJS client version"
-            text = "(version)"
-            version = html.a(text, href='#', onclick=onclick, title=tip)
-            yield ("ExtJS", version, "http://www.sencha.com")
-
-            yield ("Silk Icons", '1.3',
-                   "http://www.famfamfam.com/lab/icons/silk/")
 
     def get_index_view(self):
         from . import views
