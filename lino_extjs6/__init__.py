17dd63a75ada3e021dc017463f33de6deb665e48
Luc Saffre <luc.saffre@gmail.com>
2018-02-03 05:47:38 +0200

    http://luc.lino-framework.org/blog/2018/0203.html

--- lino_extjs6/__init__.py
+++ lino_extjs6/__init__.py
@@ -15,5 +15,5 @@ fn = join(dirname(__file__), 'setup_info.py')
 exec(compile(open(fn, "rb").read(), fn, 'exec'))
 __version__ = SETUP_INFO['version']
 
-intersphinx_urls = dict(docs="http://extjs6.lino-framework.org")
-srcref_url = 'https://github.com/lsaffre/lino_extjs6/blob/master/%s'
+# intersphinx_urls = dict(docs="http://extjs6.lino-framework.org")
+srcref_url = 'https://github.com/lino-framework/extjs6/blob/master/%s'
