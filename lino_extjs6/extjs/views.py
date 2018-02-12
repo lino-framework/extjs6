f5d2c7d718bc7775532fac49a1fb6958be29aded
khchine5 <hamzakhchine@gmail.com>
2018-02-12 23:10:37 +0100

    #1199 : Port the limit issue to Extjs6

--- lino_extjs6/extjs/views.py
+++ lino_extjs6/extjs/views.py
@@ -394,8 +394,7 @@ class ApiList(View):
                     for row in ar.sliced_data_iterator]
             total_count = ar.get_total_count()
             for row in ar.create_phantom_rows():
-                if ar.limit is not None and len(rows)+1 < ar.limit or\
-                        ar.limit == total_count + 1:
+                if ar.limit is None or len(rows) + 1 < ar.limit or ar.limit == total_count + 1:
                     d = rh.store.row2list(ar, row)
                     rows.append(d)
                 total_count += 1
