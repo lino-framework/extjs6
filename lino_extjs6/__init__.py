cdbd676752f146570a0f96d2eea35fc530cb3fd5
Luc Saffre <luc.saffre@gmail.com>
2020-07-14 12:07:36 +0300

    http://luc.lino-framework.org/blog/2020/0714.html

--- lino_extjs6/__init__.py
+++ lino_extjs6/__init__.py
@@ -8,11 +8,8 @@
 """
 
 
-import os
+from .setup_info import SETUP_INFO
 
-from os.path import join, dirname
-fn = join(dirname(__file__), 'setup_info.py')
-exec(compile(open(fn, "rb").read(), fn, 'exec'))
 __version__ = SETUP_INFO['version']
 
 # intersphinx_urls = dict(docs="http://extjs6.lino-framework.org")
--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup
 
 fn = 'lino_extjs6/setup_info.py'
-exec(compile(open(fn, "rb").read(), fn, 'exec'))
+with open(fn, "rb") as fd:
+    exec(compile(fd.read(), fn, 'exec'))
 
 if __name__ == '__main__':
     setup(**SETUP_INFO)
