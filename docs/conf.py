dfe573665636113a01c1260ce45d1b2363b77a10
Luc Saffre <luc.saffre@gmail.com>
2021-04-07 08:43:37 +0300

    https://luc.lino-framework.org/blog/2021/0407.html

--- docs/conf.py
+++ docs/conf.py
@@ -164,7 +164,7 @@ if False:
     #~ todo_include_todos = True
 
 extlinks.update(
-    ticket=('http://bugs.saffre-rumma.net/tickets/Ticket/%s', '#'))
+    ticket=('https://jane.mylino.net/#/api/tickets/AllTickets/%s', '#'))
 
 extlinks.update(
     srcref=(lino_extjs6.srcref_url, ''))
