584bdbd3284a2afbf4bb723c74720dfa04b2cc82
Luc Saffre <luc.saffre@gmail.com>
2021-04-25 19:12:37 +0300

    fix redirected urls

--- tasks.py
+++ tasks.py
@@ -3,7 +3,7 @@ ns = setup_from_tasks(
     globals(), "lino_extjs6",
     languages="en de fr et".split(),
     # tolerate_sphinx_warnings=True,
-    blogref_url = 'http://luc.lino-framework.org',
+    blogref_url = 'https://luc.lino-framework.org',
     revision_control_system='git',
     # locale_dir='lino_extjs/extjs/locale',
     cleanable_files=['docs/api/lino_extjs6.*'],
